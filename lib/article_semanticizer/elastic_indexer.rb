617af2bc8c145ad4d94e66a0c2782603c144076b
David Shorthouse <davidpshorthouse@gmail.com>
2014-09-24 09:52:34 -0400

    Clean-up elastic_indexer bin

--- lib/article_semanticizer/elastic_indexer.rb
+++ lib/article_semanticizer/elastic_indexer.rb
@@ -13,124 +13,124 @@ module ArticleSemanticizer
 
     def create
       config = {
-        :settings => {
-          :analysis => {
-            :tokenizer => {
-              :scientific_name_tokenizer => {
-                :type      => "path_hierarchy",
-                :delimiter => " ",
-                :skip      => 1
+        settings: {
+          analysis: {
+            tokenizer: {
+              scientific_name_tokenizer: {
+                type: "path_hierarchy",
+                delimiter: " ",
+                skip: 1
               }
             },
-            :filter => {
-              :autocomplete => {
-                :type     => "edgeNGram",
-                :side     => "front",
-                :min_gram => 4,
-                :max_gram => 100
+            filter: {
+              autocomplete: {
+                type: "edgeNGram",
+                side: "front",
+                min_gram: 4,
+                max_gram: 100
               },
-              :genus_abbreviation => {
-                :type     => "pattern_replace",
-                :pattern  => "(^.)[^\\s]+",
-                :replacement => "$1."
+              genus_abbreviation: {
+                type: "pattern_replace",
+                pattern: "(^.)[^\\s]+",
+                replacement: "$1."
               },
-              :vernacular_elision => {
-                :type     => "elision",
-                :articles => ["l", "m", "t"]
+              vernacular_elision: {
+                type: "elision",
+                articles: ["l", "m", "t"]
               },
-              :vernacular_stemmer => {
-                :type     => "stemmer",
-                :name     => "english"
+              vernacular_stemmer: {
+                type: "stemmer",
+                name: "english"
               }
             },
-            :analyzer => {
-              :scientific_name_index => {
-                :type         => "custom",
-                :tokenizer    => "keyword",
-                :filter       => ["lowercase", "asciifolding", :autocomplete]
+            analyzer: {
+              scientific_name_index: {
+                type: "custom",
+                tokenizer: "keyword",
+                filter: ["lowercase", "asciifolding", :autocomplete]
               },
-              :scientific_name_search => {
-                :type         => "custom",
-                :tokenizer    => "keyword",
-                :filter       => ["lowercase", "asciifolding"]
+              scientific_name_search: {
+                type: "custom",
+                tokenizer: "keyword",
+                filter: ["lowercase", "asciifolding"]
               },
-              :scientific_epithet_index => {
-                :type         => "custom",
-                :tokenizer    => "scientific_name_tokenizer",
-                :filter       => ["lowercase", "asciifolding"]
+              scientific_epithet_index: {
+                type: "custom",
+                tokenizer: "scientific_name_tokenizer",
+                filter: ["lowercase", "asciifolding"]
               },
-              :vernacular_name_index => {
-                :type         => "custom",
-                :tokenizer    => "standard",
-                :filter       => ["lowercase", "asciifolding", :vernacular_elision, :vernacular_stemmer, :autocomplete]
+              vernacular_name_index: {
+                type: "custom",
+                tokenizer: "standard",
+                filter: ["lowercase", "asciifolding", :vernacular_elision, :vernacular_stemmer, :autocomplete]
               },
-              :vernacular_name_search => {
-                :type         => "custom",
-                :tokenizer    => "standard",
-                :filter       => ["lowercase", "asciifolding", :vernacular_elision, :vernacular_stemmer]
+              vernacular_name_search: {
+                type: "custom",
+                tokenizer: "standard",
+                filter: ["lowercase", "asciifolding", :vernacular_elision, :vernacular_stemmer]
               }
             }
           }
         },
-        :mappings => {
-          :scientific => {
-            :properties => {
-              :id => { :type => 'integer', :index => 'not_analyzed' },
-              :name => { :type => 'string', :search_analyzer => :scientific_name_search, :index_analyzer => :scientific_name_index }
+        mappings: {
+          scientific: {
+            properties: {
+              id: { type: 'integer', index: 'not_analyzed' },
+              name: { type: 'string', search_analyzer: :scientific_name_search, index_analyzer: :scientific_name_index }
             }
           },
-          :vernacular => {
-            :properties => {
-              :id => { :type => 'integer', :index => 'not_analyzed' },
-              :name => { :type => 'string', :search_analyzer => :vernacular_name_search, :index_analyzer => :vernacular_name_index }
+          vernacular: {
+            properties: {
+              id: { type: 'integer', index: 'not_analyzed' },
+              name: { type: 'string', search_analyzer: :vernacular_name_search, index_analyzer: :vernacular_name_index }
             }
           },
-          :article => {
-            :properties => {
-              :id => { :type => 'integer', :index => 'not_analyzed' },
-              :doi => { :type => 'string', :index => 'not_analyzed' },
-              :year => { :type => 'integer' },
-              :pdf => { :type => 'string', :index => 'not_analyzed' },
-              :txt => { :type => 'string', :index => 'not_analyzed' },
-              :jpg => { :type => 'string', :index => 'not_analyzed' },
-              :citation => {
-                :properties => {
-                  :content => { :type => 'string', :analyzer => 'standard' },
-                  :scientific_names => { :type => 'string', :search_analyzer => :scientific_name_search, :index_analyzer => :scientific_name_search, :omit_norms => true },
-                  :vernacular_names => { 
-                    :properties => {
-                      :name => { :type => 'string', :search_analyzer => :vernacular_name_search, :index_analyzer => :vernacular_name_index, :omit_norms => true },
-                      :language => { :type => 'string', :index => 'not_analyzed'}
+          article: {
+            properties: {
+              id: { type: 'integer', index: 'not_analyzed' },
+              doi: { type: 'string', index: 'not_analyzed' },
+              year: { type: 'integer' },
+              pdf: { type: 'string', index: 'not_analyzed' },
+              txt: { type: 'string', index: 'not_analyzed' },
+              jpg: { type: 'string', index: 'not_analyzed' },
+              citation: {
+                properties: {
+                  content: { type: 'string', analyzer: 'standard' },
+                  scientific_names: { type: 'string', search_analyzer: :scientific_name_search, index_analyzer: :scientific_name_search, omit_norms: true },
+                  vernacular_names: { 
+                    properties: {
+                      name: { type: 'string', search_analyzer: :vernacular_name_search, index_analyzer: :vernacular_name_index, omit_norms: true },
+                      language: { type: 'string', index: 'not_analyzed'}
                     }
                   }
                 }
               },
-              :abstract => {
-                :properties => {
-                  :content => { :type => 'string', :analyzer => 'standard' },
-                  :scientific_names => { :type => 'string', :search_analyzer => :scientific_name_search, :index_analyzer => :scientific_name_search, :omit_norms => true },
-                  :vernacular_names => { 
-                    :properties => {
-                      :name => { :type => 'string', :search_analyzer => :vernacular_name_search, :index_analyzer => :vernacular_name_index, :omit_norms => true },
-                      :language => { :type => 'string', :index => 'not_analyzed'}
+              abstract: {
+                properties: {
+                  content: { type: 'string', analyzer: 'standard' },
+                  scientific_names: { type: 'string', search_analyzer: :scientific_name_search, index_analyzer: :scientific_name_search, omit_norms: true },
+                  vernacular_names: { 
+                    properties: {
+                      name: { type: 'string', search_analyzer: :vernacular_name_search, index_analyzer: :vernacular_name_index, omit_norms: true },
+                      language: { type: 'string', index: 'not_analyzed'}
                     }
                   }
                 }
               },
-              :full_text => {
-                :properties => {
-                  :content => { :type => 'string', :analyzer => 'standard' },
-                  :scientific_names => { :type => 'string', :search_analyzer => :scientific_name_search, :index_analyzer => :scientific_name_search, :omit_norms => true },
-                  :vernacular_names => { 
-                    :properties => {
-                      :name => { :type => 'string', :search_analyzer => :vernacular_name_search, :index_analyzer => :vernacular_name_index, :omit_norms => true },
-                      :language => { :type => 'string', :index => 'not_analyzed'}
+              full_text: {
+                properties: {
+                  content: { type: 'string', analyzer: 'standard' },
+                  scientific_names: { type: 'string', search_analyzer: :scientific_name_search, index_analyzer: :scientific_name_search, omit_norms: true },
+                  vernacular_names: { 
+                    properties: {
+                      name: { type: 'string', search_analyzer: :vernacular_name_search, index_analyzer: :vernacular_name_index, omit_norms: true },
+                      language: { type: 'string', index: 'not_analyzed'}
                     }
                   },
-                  :places => {
-                    :properties => {
-                      :name => { :type => 'string', :analyzer => 'standard' },
-                      :location => { :type => 'geo_point', :lat_lon => true }
+                  places: {
+                    properties: {
+                      name: { type: 'string', analyzer: 'standard' },
+                      location: { type: 'geo_point', lat_lon: true }
                     }
                   }
                 }
@@ -144,7 +144,7 @@ module ArticleSemanticizer
 
     def import_scientific_names
       counter = 0
-      ResolvedCanonicalForm.find_in_batches(:batch_size => 1_000) do |group|
+      ResolvedCanonicalForm.find_in_batches(batch_size: 1_000) do |group|
         scientific_names = []
         group.each do |name|
           scientific_names << { index: {
@@ -155,7 +155,7 @@ module ArticleSemanticizer
                                 }
                               }
         end
-        @client.bulk :index => ArticleSemanticizer::Config.elastic_index, :type => 'scientific', :body => scientific_names
+        @client.bulk index: ArticleSemanticizer::Config.elastic_index, type: 'scientific', body: scientific_names
         counter += scientific_names.size
         puts "Added #{counter} scientific names"
       end
@@ -163,7 +163,7 @@ module ArticleSemanticizer
 
     def import_vernacular_names
       counter = 0
-      Vernacular.find_in_batches(:batch_size => 1_000) do |group|
+      Vernacular.find_in_batches(batch_size: 1_000) do |group|
         vernacular_names = []
         group.each do |name|
           vernacular_names << { index: {
@@ -174,7 +174,7 @@ module ArticleSemanticizer
                                 }
                               }
         end
-        @client.bulk :index => ArticleSemanticizer::Config.elastic_index, :type => 'vernacular', :body => vernacular_names
+        @client.bulk index: ArticleSemanticizer::Config.elastic_index, type: 'vernacular', body: vernacular_names
         counter += vernacular_names.size
         puts "Added #{counter} vernacular names"
       end
@@ -182,7 +182,7 @@ module ArticleSemanticizer
 
     def import_articles
       counter = 0
-      Article.find_in_batches(:batch_size => 50) do |group|
+      Article.find_in_batches(batch_size: 50) do |group|
         articles = []
         group.each do |article|
           if !article.doi.nil? && !article.citation.nil?
@@ -220,14 +220,14 @@ module ArticleSemanticizer
                         }
           end
         end
-        @client.bulk :index => ArticleSemanticizer::Config.elastic_index, :type => 'article', :body => articles
+        @client.bulk index: ArticleSemanticizer::Config.elastic_index, type: 'article', body: articles
         counter += articles.size
         puts "Added #{counter} articles"
       end
     end
 
     def refresh
-      @tire.refresh
+      @client.indices.refresh index: ArticleSemanticizer::Config.elastic_index
     end
 
   end
